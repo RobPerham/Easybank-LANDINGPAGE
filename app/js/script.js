545da2ba422681cff8063ef130e1406fb3dbd73f
Rob Perham <141490769+RobPerham@users.noreply.github.com>
2023-10-31 20:01:37 +0000

    overlay / amitmation on burger menu
    okay only thing i need the fix the overlay at the started of the website

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const btnHamburger = document.querySelector('#btnHamburger ');
+const header = document.querySelector('header');
+const overlay = document.querySelector('.overlay')
+
+
+btnHamburger.addEventListener('click', function () {//close burger menu
+  console.log('click hamburger')
+
+  if (header.classList.contains('open')) {
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+  }
+  else {//OPEN hamburger menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('.overlay')
+
+  }
+
+});
